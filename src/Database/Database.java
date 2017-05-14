e621e0a692c4cd87a126751a285fd2cf1e779339
UNSWAT <adriantsoi@192.168.0.20>
2017-05-14 13:50:43 +1000

    Added Preet's 'Database' java files and 'memberLogin2'

---
+++ src/Database/Database.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Preet
+ */
+public class Database {
+    
+    public static Connection con;
+    public static String database = "jdbc:h2:./MainDatabase;create=true";
+    
+    public static void setDatabase(String database){
+        Database.database = database;
+    }
+    
+    public static void openConnection(){
+        try{
+            con = DriverManager.getConnection(database);
+            System.out.println("Connected to Database " + database);
+        }
+        catch (SQLException ex){
+            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    
+    public static void closeConnection(){
+        try{
+            if(con != null){
+                con.close();
+           }
+            System.out.println("Disconnected from Database " + database);
+        } catch (SQLException ex){
+            Logger.getLogger(Database.class.getName()).log(Level.SEVERE,null,ex);    
+        }
+            
+           
+        }
+    
+    
+    
+    
+    }
+    
+
---
+++ src/memberLogin2/LoginController.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package memberLogin2;
+
+
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import Database.Database;
+import static Database.Database.con;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * FXML Controller class
+ *
+ * @author Preet
+ */
+public class LoginController implements Initializable {
+
+    @FXML
+    private TextField email;
+    @FXML
+    private TextField password;
+    @FXML
+    private Button signin;
+    @FXML
+    private Button register;
+    @FXML
+    private Label setLabel;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void emailAddress(ActionEvent event) {
+    }
+
+    @FXML
+    private void enterPassword(ActionEvent event) {
+    }
+
+    @FXML
+    private void clickLogin(ActionEvent event) throws SQLException {
+       PreparedStatement ps;
+       Database.openConnection();
+       ps = con.prepareStatement("SELECT USERNAME,PASSWORD from PUBLIC.USER WHERE USERNAME = ? and PASSWORD = ?;");
+       ps.setString(1,email.getText());
+       ps.setString(2,password.getText());
+       ResultSet result = ps.executeQuery();
+       if(result.next()){
+           setLabel.setText("Login Successfully");
+       }
+       else {
+           setLabel.setText("Invalid Username or Password");
+       }
+       Database.closeConnection();
+       
+        
+    
+    }
+
+    @FXML
+    private void clickRegister(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/signup/signupFXML.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+               
+    }
+    
+}
