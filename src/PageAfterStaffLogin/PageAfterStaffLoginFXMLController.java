5411afda77eb1148637d7a4701447b54f7b41788
frank2647 <frankyifei0511@gmail.com>
2017-05-15 02:34:01 +1000

    Frank

--- src/PageAfterStaffLogin/PageAfterStaffLoginFXMLController.java
+++ src/PageAfterStaffLogin/PageAfterStaffLoginFXMLController.java
@@ -5,9 +5,17 @@
  */
 package PageAfterStaffLogin;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 
 /**
  * FXML Controller class
@@ -16,9 +24,18 @@ import javafx.fxml.Initializable;
  */
 public class PageAfterStaffLoginFXMLController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private void clickCorporateCustomer(ActionEvent event)throws IOException{
+        
+        System.out.println("going to Corporate Customer");
+        Parent root = FXMLLoader.load(getClass().getResource("ClickCorporateCustomer.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = PageAfterStaffLogin.getStage(); 
+        stage.setScene(scene);
+        stage.show();
+        };
+    
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
