69a4280ee2cb1f5af510f4179670d5f7383efa5a
frank2647 <frankyifei0511@gmail.com>
2017-05-27 00:49:19 +1000

    Frank, AddStaffAccount works

--- src/StaffLogin/AddStaffAccountController.java
+++ src/StaffLogin/AddStaffAccountController.java
@@ -7,6 +7,8 @@ package StaffLogin;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -14,9 +16,18 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import Database.Database;
+import static Database.Database.con;
+import java.sql.ResultSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+
 
 /**
  * FXML Controller class
@@ -32,8 +43,107 @@ public class AddStaffAccountController implements Initializable {
     @FXML
     private TextField username;
     @FXML
-    private TextField password;
+    private PasswordField password;
+    @FXML
+    private PasswordField password2;
+    @FXML
+    private Label errorMessage;
 
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+     @FXML
+    private void createAccount(ActionEvent event) throws SQLException {
+        try {     
+            boolean check = false;
+            PreparedStatement insertMember = null;
+            PreparedStatement usernameCheck= null;
+            Database.openConnection();
+            
+            String fname = firstName.getText(); 
+            
+            String lname = lastName.getText();
+            
+            String userName = username.getText();
+            
+            String password1 = password.getText();
+            
+            String passwordConfirm = password2.getText();
+            
+            
+            
+            while (check == false) {
+                 if (userName.equals("")){
+                 errorMessage.setText("Please enter a username");
+                 check =true;
+            }             
+             else if (fname.equals("")){
+                 errorMessage.setText("Please enter your first name");
+                 check =true;
+            }
+             else if (lname.equals("")){
+                 errorMessage.setText("Please enter your last name");
+                 check =true;
+            }
+            else if (password1.equals("")){
+                 errorMessage.setText("Please enter a valid password");
+                 check =true;
+            }
+             else if (passwordConfirm.equals("")){
+                 errorMessage.setText("Please confirm your password");
+                 check =true;
+            }
+            else if (!password1.equals(passwordConfirm)){
+                  errorMessage.setText("Your passwords are not identical. Please try again.");
+                  check =true;
+              }              
+              else if (password1.equals(passwordConfirm)){
+                  usernameCheck =con.prepareStatement("SELECT USERNAME FROM PUBLIC.STAFF WHERE USERNAME = ?");
+                  usernameCheck.setString(1,userName);
+                  ResultSet allUsername = usernameCheck.executeQuery();
+                  if (allUsername.next()){
+                      errorMessage.setText("That username has already been taken. Please choose another one");
+                      check = true;
+                  }
+                  
+                  else {              
+                      insertMember = con.prepareStatement("INSERT INTO PUBLIC.STAFF(USERNAME,PASSWORD,FIRST_NAME,LAST_NAME) VALUES(?,?,?,?)");
+                            insertMember.setString(1, userName);
+                            insertMember.setString(2, password1);
+                            insertMember.setString(3,fname);
+                            insertMember.setString(4, lname); 
+                            insertMember.execute();
+                            insertMember.close(); 
+                            check =true;
+                            
+                            Database.closeConnection();
+                            try {
+                                Parent root;
+                                root = FXMLLoader.load(getClass().getResource("StaffList.fxml"));
+                                Scene scene = new Scene(root);
+                                Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+                                stage.setScene(scene);
+                                stage.show();
+
+                            } catch (IOException ex) {
+                                Logger.getLogger(AddStaffAccountController.class.getName()).log(Level.SEVERE, null, ex);
+                            }
+                    }     
+              }        
+        }
+     
+            } catch (SQLException ex) {
+               Logger.getLogger(AddStaffAccountController.class.getName()).log(Level.SEVERE, null, ex);
+               System.out.println("SQL error");
+           }  
+    }
+            
+    
     @FXML
     private void CancelCreateStaff(ActionEvent event)throws IOException{
         
@@ -43,17 +153,5 @@ public class AddStaffAccountController implements Initializable {
         Stage stage = StaffLogin.getStage(); 
         stage.setScene(scene);
         stage.show();
-        };
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-
-    @FXML
-    private void CancelCreateStaff(MouseEvent event) {
     }
-    
 }
