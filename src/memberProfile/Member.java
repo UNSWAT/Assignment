9e4d8d34391eff37f678fe6422f6a5a2c8c88c10
Yu Bernice <Yu Bernice@10.249.47.190>
2017-05-28 14:35:02 +1000

    Working on memberProfile -Bernice 28/5/17

--- src/memberProfile/Member.java
+++ src/memberProfile/Member.java
@@ -5,29 +5,23 @@
  */
 package memberProfile;
 
+import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
-import javafx.fxml.FXML;
-import javafx.scene.control.TableColumn;
+import javafx.beans.property.StringProperty;
 
 /**
  *
  * @author Yu Bernice
  */
 public class Member {
-     private SimpleStringProperty memberusername,password,fname,lname,email,mobile,
-             haddress,hsuburb,hpostcode,hstate,waddress,wsuburb,wpostcode,wstate,
-             ccnumber,ccexpirymonth,ccexpiryyear,corporatemember,companyname,
-             membertype;
-    //private SimpleIntegerProperty postcodefrom, postcodeto,Quota, Offerid;
-    
-    
-    
-    
+
+    private SimpleStringProperty memberusername, password, fname, lname, email, mobile,
+            haddress, hsuburb, hstate, waddress, wsuburb, wstate;
+    private SimpleIntegerProperty hpostcode, wpostcode ;
+
     public Member(String memberusername, String password, String fname, String lname, String email, String mobile,
-            String haddress, String hsuburb,String hpostcode,String hstate,String waddress,String wsuburb,
-            String wpostcode,String wstate,String ccnumber ,String ccexpirymonth,String ccexpiryyear,String corporatemember,
-             String companyname, String membertype) {
+            String haddress, String hsuburb, int hpostcode, String hstate, String waddress, String wsuburb, int wpostcode, String wstate) {
         this.memberusername = new SimpleStringProperty(memberusername);
         this.password = new SimpleStringProperty(password);
         this.fname = new SimpleStringProperty(fname);
@@ -36,181 +30,181 @@ public class Member {
         this.mobile = new SimpleStringProperty(mobile);
         this.haddress = new SimpleStringProperty(haddress);
         this.hsuburb = new SimpleStringProperty(hsuburb);
-        this.hpostcode = new SimpleStringProperty(hpostcode);
+        this.hpostcode = new SimpleIntegerProperty(hpostcode);
         this.hstate = new SimpleStringProperty(hstate);
         this.waddress = new SimpleStringProperty(waddress);
         this.wsuburb = new SimpleStringProperty(wsuburb);
-        this.wpostcode = new SimpleStringProperty(wpostcode);
+        this.wpostcode = new SimpleIntegerProperty(wpostcode);
         this.wstate = new SimpleStringProperty(wstate);
-        this.ccnumber = new SimpleStringProperty(ccnumber);
-        this.ccexpirymonth = new SimpleStringProperty(ccexpirymonth);
-        this.ccexpiryyear = new SimpleStringProperty(ccexpiryyear); 
-        this.corporatemember = new SimpleStringProperty(corporatemember);
-        this.companyname = new SimpleStringProperty(companyname);
-        this.membertype = new SimpleStringProperty(membertype);
-        
-        
-        
+
     }
 
-    public SimpleStringProperty getMemberusername() {
+    public int getHpostcode() {
+        return hpostcode.get();
+    }
+    
+    public int getWpostcode() {
+        return wpostcode.get();
+    }
+    
+    public String getMemberusername() {
+        return memberusername.get();
+    }
+
+    public String getPassword() {
+        return password.get();
+    }
+
+    public String getFname() {
+        return fname.get();
+    }
+
+    public String getLname() {
+        return lname.get();
+    }
+
+    public String getEmail() {
+        return email.get();
+    }
+
+    public String getMobile() {
+        return mobile.get();
+    }
+
+    public String getHaddress() {
+        return haddress.get();
+    }
+
+    public String getHsuburb() {
+        return hsuburb.get();
+    }
+
+    public String getHstate() {
+        return hstate.get();
+    }
+
+    public String getWaddress() {
+        return waddress.get();
+    }
+
+    public String getWsuburb() {
+        return wsuburb.get();
+    }
+
+    public String getWstate() {
+        return wstate.get();
+    }
+
+    public void setWPostcode(int value) {
+        wpostcode.set(value);
+    }
+    
+    public void setHPostcode(int value) {
+        hpostcode.set(value);
+    }
+    
+    public void setMemberusername(String value) {
+        memberusername.set(value);
+    }
+
+    public void setPassword(String value) {
+        password.set(value);
+    }
+
+    public void setFname(String value) {
+        fname.set(value);
+    }
+
+    public void setLname(String value) {
+        lname.set(value);
+    }
+
+    public void setEmail(String value) {
+        email.set(value);
+    }
+
+    public void setMobile(String value) {
+        mobile.set(value);
+    }
+
+    public void setHaddress(String value) {
+        haddress.set(value);
+    }
+
+    public void setHsuburb(String value) {
+        hsuburb.set(value);
+    }
+
+    public void setHstate(String value) {
+        hstate.set(value);
+    }
+
+    public void setWaddress(String value) {
+        waddress.set(value);
+    }
+
+    public void setWsuburb(String value) {
+        wsuburb.set(value);
+    }
+
+    public void setWstate(String value) {
+        wstate.set(value);
+    }
+
+    public StringProperty memberusername() {
         return memberusername;
     }
 
-    public void setMemberusername(SimpleStringProperty memberusername) {
-        this.memberusername = memberusername;
-    }
-
-    public SimpleStringProperty getPassword() {
+    public StringProperty password() {
         return password;
     }
 
-    public void setPassword(SimpleStringProperty password) {
-        this.password = password;
-    }
-
-    public SimpleStringProperty getFname() {
+    public StringProperty fname() {
         return fname;
     }
 
-    public void setFname(SimpleStringProperty fname) {
-        this.fname = fname;
-    }
-
-    public SimpleStringProperty getLname() {
+    public StringProperty lname() {
         return lname;
     }
 
-    public void setLname(SimpleStringProperty lname) {
-        this.lname = lname;
-    }
-
-    public SimpleStringProperty getEmail() {
+    public StringProperty email() {
         return email;
     }
 
-    public void setEmail(SimpleStringProperty email) {
-        this.email = email;
-    }
-
-    public SimpleStringProperty getMobile() {
+    public StringProperty mobile() {
         return mobile;
     }
 
-    public void setMobile(SimpleStringProperty mobile) {
-        this.mobile = mobile;
-    }
-
-    public SimpleStringProperty getHaddress() {
+    public StringProperty haddress() {
         return haddress;
     }
 
-    public void setHaddress(SimpleStringProperty haddress) {
-        this.haddress = haddress;
-    }
-
-    public SimpleStringProperty getHsuburb() {
+    public StringProperty hsuburb() {
         return hsuburb;
     }
 
-    public void setHsuburb(SimpleStringProperty hsuburb) {
-        this.hsuburb = hsuburb;
-    }
-
-    public SimpleStringProperty getHpostcode() {
-        return hpostcode;
-    }
-
-    public void setHpostcode(SimpleStringProperty hpostcode) {
-        this.hpostcode = hpostcode;
-    }
-
-    public SimpleStringProperty getHstate() {
+    public StringProperty hstate() {
         return hstate;
     }
 
-    public void setHstate(SimpleStringProperty hstate) {
-        this.hstate = hstate;
-    }
-
-    public SimpleStringProperty getWaddress() {
+    public StringProperty waddress() {
         return waddress;
     }
 
-    public void setWaddress(SimpleStringProperty waddress) {
-        this.waddress = waddress;
-    }
-
-    public SimpleStringProperty getWsuburb() {
+    public StringProperty wsuburb() {
         return wsuburb;
     }
 
-    public void setWsuburb(SimpleStringProperty wsuburb) {
-        this.wsuburb = wsuburb;
-    }
-
-    public SimpleStringProperty getWpostcode() {
-        return wpostcode;
-    }
-
-    public void setWpostcode(SimpleStringProperty wpostcode) {
-        this.wpostcode = wpostcode;
-    }
-
-    public SimpleStringProperty getWstate() {
+    public StringProperty wstate() {
         return wstate;
     }
-
-    public void setWstate(SimpleStringProperty wstate) {
-        this.wstate = wstate;
-    }
-
-    public SimpleStringProperty getCcnumber() {
-        return ccnumber;
-    }
-
-    public void setCcnumber(SimpleStringProperty ccnumber) {
-        this.ccnumber = ccnumber;
-    }
-
-    public SimpleStringProperty getCcexpirymonth() {
-        return ccexpirymonth;
-    }
-
-    public void setCcexpirymonth(SimpleStringProperty ccexpirymonth) {
-        this.ccexpirymonth = ccexpirymonth;
-    }
-
-    public SimpleStringProperty getCcexpiryyear() {
-        return ccexpiryyear;
-    }
-
-    public void setCcexpiryyear(SimpleStringProperty ccexpiryyear) {
-        this.ccexpiryyear = ccexpiryyear;
-    }
-
-    public SimpleStringProperty getCorporatemember() {
-        return corporatemember;
-    }
-
-    public void setCorporatemember(SimpleStringProperty corporatemember) {
-        this.corporatemember = corporatemember;
-    }
-
-    public SimpleStringProperty getCompanyname() {
-        return companyname;
-    }
-
-    public void setCompanyname(SimpleStringProperty companyname) {
-        this.companyname = companyname;
-    }
-
-    public SimpleStringProperty getMembertype() {
-        return membertype;
-    }
-
-    public void setMembertype(SimpleStringProperty membertype) {
-        this.membertype = membertype;
+    
+    public IntegerProperty wpostcode() {
+        return wpostcode;
     }
     
+    public IntegerProperty hpostcode() {
+        return hpostcode;
+    }
+
 }
--- src/memberProfile/memberProfileController.java
+++ src/memberProfile/memberProfileController.java
@@ -11,17 +11,24 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import offerPosting.Offer;
 import Database.Database;
 import static Database.Database.con;
+import java.io.IOException;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 import memberLogin2.User;
 
 /**
@@ -33,8 +40,6 @@ public class memberProfileController implements Initializable {
 
     @FXML
     private TableView<Member> members;
-    @FXML
-    private TableView<Member> members2;
     @FXML
     private TableColumn<Member, String> memberusername;
     @FXML
@@ -52,7 +57,7 @@ public class memberProfileController implements Initializable {
     @FXML
     private TableColumn<Member, String> hsuburb;
     @FXML
-    private TableColumn<Member, String> hpostcode;
+    private TableColumn<Member, Integer> hpostcode;
     @FXML
     private TableColumn<Member, String> hstate;
     @FXML
@@ -60,97 +65,42 @@ public class memberProfileController implements Initializable {
     @FXML
     private TableColumn<Member, String> wsuburb;
     @FXML
-    private TableColumn<Member, String> wpostcode;
+    private TableColumn<Member, Integer> wpostcode;
     @FXML
     private TableColumn<Member, String> wstate;
-    @FXML
-    private TableColumn<Member, String> ccnumber;
-    @FXML
-    private TableColumn<Member, String> ccexpirymonth;
-    @FXML
-    private TableColumn<Member, String> ccexpiryyear;
-    @FXML
-    private TableColumn<Member, String> corporatemember;
-    @FXML
-    private TableColumn<Member, String> companyname;
-    @FXML
-    private TableColumn<Member, String> membertype;
-    
-    
-   
+
     private ObservableList<Member> data;
     private PreparedStatement getMembers;
     @FXML
     private Label username;
-    
-    @FXML
-    private Label memberPassword;
-    
-    @FXML
-    private Label fName;
-    @FXML
-    private Label lName;
-    @FXML
-    private Label emailAddress;
-    @FXML
-    private Label mobileNo;
-    @FXML
-    private Label homeAddress;
-    @FXML
-    private Label homeSuburb;
-    @FXML
-    private Label homePostcode;
-    @FXML
-    private Label homeState;
-    @FXML
-    private Label workSddress;
-    @FXML
-    private Label workSuburb;
-    @FXML
-    private Label workPostcode;
-    @FXML
-    private Label workState;
-    @FXML
-    private Label ccNumber;
-    @FXML
-    private Label ccExpiryMonth;
-    @FXML
-    private Label ccExpiryYear;
-    @FXML
-    private Label corporateMember;
-    @FXML
-    private Label companyName;
-    @FXML
-    private Label memberType;
-    
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         username.setText(User.getUsername());
-        try{
+        String user = username.getText();
+        try {
             Database.openConnection();
-            data=FXCollections.observableArrayList();
-            getMembers = con.prepareStatement("select MEMBER_USERNAME,MEMBER_PASSWORD, "
-                    + "FISRST_NAME, LAST_NAME, EMAIL_ADDRESS,MOBILE_NO, HOME_ADDRESS, HOME_SUBURB, "
-                    + "HOME_POSTCODE, HOME_STATE, WORK_ADDRESS, WORK_SUBURB,WORKPOSTCODE,WORKSTATE,"
-                    + "CCNUMBER,CCEXPRIRY_MONTH, CCEXPIRY_YEAR, CORPORATE_MEMBER, "
-                    + "COMPANY_NAME,MEMBER_TYPE from MEMBERS ");
+            data = FXCollections.observableArrayList();
+            getMembers = con.prepareStatement("SELECT MEMBER_USERNAME,MEMBER_PASSWORD, "
+                    + "FIRST_NAME, LAST_NAME, EMAIL_ADDRESS,MOBILE_NO, HOME_ADDRESS, HOME_SUBURB, "
+                    + "HOME_POSTCODE, HOME_STATE, WORK_ADDRESS, WORK_SUBURB,WORK_POSTCODE,WORK_STATE FROM MEMBERS ");
+            getMembers.setString(1, user);
+            getMembers.executeQuery();
 //            getOffers.setInt(1,OtherStaticVariables.getQuota() );
 //            getOffers.setString(2, username.getText());
 //            getOffers.setInt(3, OtherStaticVariables.getPostcodefrom());
 //            getOffers.setString(4, OtherStaticVariables.getTimefrom());
-           
+
             ResultSet rs = getMembers.executeQuery();
-            
-            while(rs.next()){
-                data.add(new Member(rs.getString("MEMBER_USERNAME"),rs.getString("MEMBER_PASSWORD"),
-                        rs.getString("FISRST_NAME"),rs.getString("LAST_NAME"),rs.getString("EMAIL_ADDRESS"),
-                        rs.getString("MOBILE_NO"),rs.getString("HOME_ADDRESS"),rs.getString("HOME_SUBURB"),
-                        rs.getString("HOME_POSTCODE"),rs.getString("HOME_STATE"),rs.getString("WORK_ADDRESS"),rs.getString("WORK_SUBURB"),
-                rs.getString("WORKPOSTCODE"),rs.getString("WORKSTATE"),rs.getString("CCNUMBER"),rs.getString("CCEXPRIRY_MONTH"),
-                rs.getString("CCEXPIRY_YEAR"),rs.getString("CORPORATE_MEMBER"),rs.getString("COMPANY_NAME"),rs.getString("MEMBER_TYPE")));
+
+            while (rs.next()) {
+                data.add(new Member(rs.getString("MEMBER_USERNAME"), rs.getString("MEMBER_PASSWORD"),
+                        rs.getString("FIRST_NAME"), rs.getString("LAST_NAME"), rs.getString("EMAIL_ADDRESS"),
+                        rs.getString("MOBILE_NO"), rs.getString("HOME_ADDRESS"), rs.getString("HOME_SUBURB"), 
+                        rs.getInt("HOME_POSTCODE"),rs.getString("HOME_STATE"), rs.getString("WORK_ADDRESS"), 
+                        rs.getString("WORK_SUBURB"), rs.getInt("WORK_POSTCODE"), rs.getString("WORK_STATE")));
             }
-            
+
 //            ResultSet result = getOffers.executeQuery();
 //            while(result.next()){
 //                String userName = result.getString("MEMBER_USERNAME");
@@ -164,58 +114,67 @@ public class memberProfileController implements Initializable {
 //                System.out.println(offer.getMemberUsername());
 //                System.out.println(offer.getTimefrom());
 //                data.add(offer);
-           //}
-
+            //}
             memberusername.setCellValueFactory(new PropertyValueFactory<>("MEMBER_USERNAME"));
             password.setCellValueFactory(new PropertyValueFactory<>("MEMBER_PASSWORD"));
-            fname.setCellValueFactory(new PropertyValueFactory<>("FISRST_NAME"));
+            fname.setCellValueFactory(new PropertyValueFactory<>("FIRST_NAME"));
             lname.setCellValueFactory(new PropertyValueFactory<>("LAST_NAME"));
             email.setCellValueFactory(new PropertyValueFactory<>("EMAIL_ADDRESS"));
             mobile.setCellValueFactory(new PropertyValueFactory<>("MOBILE_NO"));
             haddress.setCellValueFactory(new PropertyValueFactory<>("HOME_ADDRESS"));
             hsuburb.setCellValueFactory(new PropertyValueFactory<>("HOME_SUBURB"));
-            hpostcode.setCellValueFactory(new PropertyValueFactory<>("HOME_POSTCODE"));
+            //hpostcode.setCellValueFactory(new PropertyValueFactory<>("HOME_POSTCODE"));
             hstate.setCellValueFactory(new PropertyValueFactory<>("HOME_STATE"));
             waddress.setCellValueFactory(new PropertyValueFactory<>("WORK_ADDRESS"));
             wsuburb.setCellValueFactory(new PropertyValueFactory<>("WORK_SUBURB"));
-            wpostcode.setCellValueFactory(new PropertyValueFactory<>("WORKPOSTCODE"));
-            wstate.setCellValueFactory(new PropertyValueFactory<>("WORKSTATE"));
-            ccnumber.setCellValueFactory(new PropertyValueFactory<>("CCNUMBER"));
-            ccexpirymonth.setCellValueFactory(new PropertyValueFactory<>("CCEXPRIRY_MONTH"));
-            ccexpiryyear.setCellValueFactory(new PropertyValueFactory<>("CCEXPIRY_YEAR"));
-            corporatemember.setCellValueFactory(new PropertyValueFactory<>("CORPORATE_MEMBER"));
-            companyname.setCellValueFactory(new PropertyValueFactory<>("COMPANY_NAME"));
-            membertype.setCellValueFactory(new PropertyValueFactory<>("MEMBER_TYPE"));
-            
-            
+            //wpostcode.setCellValueFactory(new PropertyValueFactory<>("WORK_POSTCODE"));
+            wstate.setCellValueFactory(new PropertyValueFactory<>("WORK_STATE"));
+
             members.setItems(null);
             members.setItems(data);
-            
-            members.setItems(null);
-            members2.setItems(data);
-            
-                    
-            
-        }catch(SQLException se){
+
+        } catch (SQLException se) {
             System.out.println(se);
         }
         Database.closeConnection();
-    }  
-  
+    }
 
     @FXML
     private void Home(ActionEvent event) {
-    }
+        try {
+            Pane root;
 
-    private void MyAgreements(ActionEvent event) {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/memberType/memberBoth.fxml"));
+
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene((Pane) loader.load()));
+
+            stage.show();
+        } catch (IOException ex) {
+            Logger.getLogger(memberProfileController.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Page Error");
+        }
     }
 
     @FXML
     private void logOut(ActionEvent event) {
+        try {
+            Pane root;
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/memberLogin2/Login.fxml"));
+
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene((Pane) loader.load()));
+
+            stage.show();
+        } catch (IOException ex) {
+            Logger.getLogger(memberProfileController.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Page Error");
+        }
     }
+}
 
-
-    /*@FXML
+/*@FXML
     private void edit(ActionEvent event) {        
             Offer currentOffer = (Offer)members.getSelectionModel().getSelectedItem();
             int offerID = currentOffer.getOfferid();
@@ -237,6 +196,3 @@ public class memberProfileController implements Initializable {
             Logger.getLogger(memberProfileController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }*/
-    
-    
-}
